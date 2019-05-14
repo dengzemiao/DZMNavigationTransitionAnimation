91732c9ad9bc619cfd34af70321626f361b1845a
dengzemiao <dengzemiao@dengzemiaodemacbook-pro.local>
2019-05-14 10:29:25 +0800

    .

--- DZMNavigationTransitionAnimation/DZMNavigationTransitionAnimation/UINavigationController+DZM.m
+++ DZMNavigationTransitionAnimation/DZMNavigationTransitionAnimation/UINavigationController+DZM.m
@@ -50,4 +50,14 @@
     objc_setAssociatedObject(self, @selector(dzm_interactivePopDisabled), @(disabled), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
+- (BOOL)dzm_prefersNavigationBarHidden {
+    
+    return [objc_getAssociatedObject(self, _cmd) boolValue];
+}
+
+- (void)setDzm_prefersNavigationBarHidden:(BOOL)hidden {
+    
+    objc_setAssociatedObject(self, @selector(dzm_prefersNavigationBarHidden), @(hidden), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
 @end
